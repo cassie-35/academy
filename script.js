493a2e7da12a01712432f353fe3296344004b4ca
Cassandra Damian <cndv35@gmail.com>
2025-03-04 10:12:17 -0500

    t

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const primerElemento = carrusel.children[0];
 
         carrusel.style.transition = "transform 0.5s ease-in-out";
-        carrusel.style.transform = `translateX(-${direccion * 33.33}%)`;
+        carrusel.style.transform = translateX(-${direccion * 33.33}%);
 
         setTimeout(() => {
             carrusel.style.transition = "none";
